0cae6222657dea8c81a879f4b111e85ef024c321
Nate Denys <nate_denys@yahoo.com>
2021-08-10 11:54:59 -0700

    Initial Commit
    Create-react-app utilizing React Most Wanted Template for Project structure layout

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -5,7 +5,7 @@ import { Helmet } from 'react-helmet'
 import Paper from '@material-ui/core/Paper'
 import { Scrollbars } from 'react-custom-scrollbars'
 import Toolbar from '@material-ui/core/Toolbar'
-import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { ThemeProvider, createTheme } from '@material-ui/core/styles'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import Button from '@material-ui/core/Button'
 import { useHistory } from 'react-router-dom'
@@ -16,7 +16,7 @@ const ResponsiveMenu = lazy(() =>
   import('rmw-shell/lib/containers/ResponsiveMenu')
 )
 
-const theme = createMuiTheme({
+const theme = createTheme({
   palette: {
     primary: { main: '#242424' },
     secondary: {
